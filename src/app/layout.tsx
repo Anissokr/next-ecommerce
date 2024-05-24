f8fa48c32e1959ea65e07e11d56e1014ead23296
Anis <kraiemanis@gmail.com>
2024-05-24 13:44:06 +0100

    nextjs store wix integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+
+import { WixClientContextProvider } from "@/context/wixContext";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <WixClientContextProvider>
         <Navbar />
         {children}
         <Footer/>
+        </WixClientContextProvider>
         </body>
     </html>
   );
---
+++ src/hooks/useWixClient.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { wixClientContext } from "@/context/wixContext";
+import { useContext } from "react";
+
+export const useWixClient = ()=>{
+    return  useContext(wixClientContext);
+}
+
+ 
